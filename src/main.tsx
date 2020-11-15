d7ca6ca2544063eefeda3ba964b1fff939cab153
jinpei0908 <jinpei0908@gmail.com>
2020-11-15 15:05:45 +0900

    絞り込み用の検索ボックスを追加した/npm scriptを追加した

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,34 @@
+import { useState } from 'react'
 import ReactDOM from 'react-dom'
+import Input from './component/input'
 import Table from './component/table'
 import books from './data/books'
 
-const element = (
-  <>
-    <Table books={books} />
-  </>
-)
+const createRegex = (text: string): string => `.*${text}.*`
 
-ReactDOM.render(element, document.getElementById('root'))
+const RootElement = () => {
+  const [value, setValue] = useState('')
+  const [filteredBooks, setFilteredBooks] = useState(books)
+
+  return (
+    <>
+      <Input
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value)
+          setFilteredBooks(
+            books.filter(
+              (book) =>
+                book.title
+                  .toLocaleLowerCase()
+                  .match(createRegex(e.target.value)) != null
+            )
+          )
+        }}
+      />
+      <Table books={filteredBooks} />
+    </>
+  )
+}
+
+ReactDOM.render(<RootElement />, document.getElementById('root'))
